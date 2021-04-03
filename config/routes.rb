711741e044672a4db86e3e6fceebf23712543b03
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-04-03 09:48:44 +0000

    [Add] devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,29 @@
 Rails.application.routes.draw do
+  
+  devise_for :admins, controllers: {
+    sessions: 'admins/sessions',
+  }
+  
+  namespace :admins do
+    resources :users, only: [:index, :show, :edit, :update]
+    resources :genres, only: [:index, :create, :edit, :update]
+  end
+  
+  devise_for :users, controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  
+  scope module: :users do
+    root to: "homes#top"
+    get '/users/mypage' => 'users#show'
+    get "/users/unsubscribe" => "users#unsubscribe"
+    patch "/users/withdraw" => "users#withdraw"
+    get "/users/mypage/edit" => "users#edit"
+    patch "/users/mypage" => "users#update"
+    
+    resources :comments
+    resources :posts
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
