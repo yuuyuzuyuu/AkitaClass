677586a6a2c6e6b10d7280cea2ad44f68c9da6eb
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-04-04 07:00:52 +0000

    [Add]help_post

---
+++ app/helpers/users/help_posts_helper.rb
@@ -0,0 +1,2 @@
+module Users::HelpPostsHelper
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_03_091911) do
+ActiveRecord::Schema.define(version: 2021_04_04_045457) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,15 @@ ActiveRecord::Schema.define(version: 2021_04_03_091911) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_admins_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
+
+  create_table "help_posts", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "genre_id"
+    t.string "title", null: false
+    t.text "body", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -45,6 +54,11 @@ ActiveRecord::Schema.define(version: 2021_04_03_091911) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
+  create_table "want_posts", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
