af315de964bfbb804350a6ce70de7b6278111ac5
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-04-05 06:10:51 +0000

    [Fix]help_post

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,3 +1,7 @@
 class Genre < ApplicationRecord
+  
+  has_many :help_posts
+  
   validates :name, presence: true
+  
 end
--- app/models/help_post.rb
+++ app/models/help_post.rb
@@ -1,6 +1,7 @@
 class HelpPost < ApplicationRecord
 
   belongs_to :user
+  belongs_to :genre
   
   validates :title, presence: true
   validates :body, presence: true
