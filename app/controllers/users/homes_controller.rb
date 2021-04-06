b689abeb8586bc38b40fcd3e1a15afbd4983f205
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-04-06 14:45:22 +0000

    [Fix]post-show

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -1,6 +1,7 @@
 class Users::HomesController < ApplicationController
   def top
     @help_posts = HelpPost.order(created_at: :desc).limit(3)
+    @want_posts = WantPost.order(created_at: :desc).limit(3)
   end
   
   def about
