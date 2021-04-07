5f9551d4c913c3bcd65c97dc66700fcfb243f364
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-04-07 08:10:02 +0000

    [Fix]post_index

--- app/controllers/users/members_controller.rb
+++ app/controllers/users/members_controller.rb
@@ -1,30 +1,31 @@
 class Users::MembersController < ApplicationController
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     @help_posts = HelpPost.all
+    @want_posts = WantPost.all
   end
-  
-  def edit 
+
+  def edit
     @user = current_user
   end
-  
+
   def update
     @user = current_user
     if @user.update(user_params)
-      redirect_to members_mypage_path
+      redirect_to profile_path
     else
       render "edit"
     end
   end
-  
+
   def unsubscribe
   end
-  
+
   def withdraw
   end
-  
+
   private
-  
+
   def user_params
     params.require(:user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :email, :introduce, :birth_date, :phone_number, :address, :profile_image)
   end
