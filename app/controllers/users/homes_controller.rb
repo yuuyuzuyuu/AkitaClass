651f400a7189db3ef7e94464879e45af9ac5ea24
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-04-08 01:22:06 +0000

    [Fix]layout-registration and session

--- app/controllers/users/homes_controller.rb
+++ app/controllers/users/homes_controller.rb
@@ -2,6 +2,8 @@ class Users::HomesController < ApplicationController
   def top
     @help_posts = HelpPost.order(created_at: :desc).limit(3)
     @want_posts = WantPost.order(created_at: :desc).limit(3)
+    @help_ranks = HelpPost.find(HelpLike.group(:help_post_id).order('count(help_post_id) desc').limit(3).pluck(:help_post_id))
+    @want_ranks = WantPost.find(WantLike.group(:want_post_id).order('count(want_post_id) desc').limit(3).pluck(:want_post_id))
   end
 
   def about
