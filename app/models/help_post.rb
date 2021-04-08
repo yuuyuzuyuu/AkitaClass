12f70f0a2058e80df6935b45f5907d0314c9ed96
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-04-08 03:40:08 +0000

    [Fix]post-edit, post-new

--- app/models/help_post.rb
+++ app/models/help_post.rb
@@ -4,11 +4,13 @@ class HelpPost < ApplicationRecord
   belongs_to :genre
   has_many :help_comments, dependent: :destroy
   has_many :help_likes, dependent: :destroy
-
+  
   validates :title, presence: true
   validates :body, presence: true
   attachment :post_image
 
+  acts_as_taggable_on :tags
+  
   def liked_by?(user)
     help_likes.where(user_id: user.id).exists?
   end
--- app/models/want_post.rb
+++ app/models/want_post.rb
@@ -9,6 +9,8 @@ class WantPost < ApplicationRecord
   validates :body, presence: true
   attachment :post_image
   
+  acts_as_taggable_on :tags
+  
   def liked_by?(user)
     want_likes.where(user_id: user.id).exists?
   end
