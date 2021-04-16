ddaeaef00f833a5fdd0f39a9646281ec29ae8ce7
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-04-16 01:43:45 +0000

    [Fix]tag-it

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -41,16 +41,16 @@ $(document).on('turbolinks:load', function() {
     singleField: true,   // タグの一意性
  // availableTags: ['ruby', 'rails', ..]  自動補完する一覧を設定できる(※ 配列ならok)。
   });
-  let tag_count = 10 - $(".tagit-choice").length    // 登録済みのタグを数える
-  $(".ui-widget-content.ui-autocomplete-input").attr(
-    'placeholder','あと' + tag_count + '個登録できます');
+  // let tag_count = 10 - $(".tagit-choice").length    // 登録済みのタグを数える
+  // $(".ui-widget-content.ui-autocomplete-input").attr(
+  //   'placeholder','あと' + tag_count + '個登録できます');
 });
 
 // タグ入力で、placeholder を変更
 $(document).on('turbolinks:load', "keyup", '.tagit', function() {
-  let tag_count = 10 - $(".tagit-choice").length
-  $(".ui-widget-content.ui-autocomplete-input").attr(
-  'placeholder','あと' + tag_count + '個登録できます');
+  // let tag_count = 10 - $(".tagit-choice").length
+  // $(".ui-widget-content.ui-autocomplete-input").attr(
+  // 'placeholder','あと' + tag_count + '個登録できます');
 
    // Ajaxで、タグ一覧を取得
   let input = $(".ui-widget-content.ui-autocomplete-input").val();  // 変数inputに、入力値を格納
