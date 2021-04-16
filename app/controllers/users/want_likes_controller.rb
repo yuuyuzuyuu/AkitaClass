8fa7d9efd317a9e11a9c8a598cc27f9007174c06
yuuyuzuyuu <ym.74.mn@gmail.com>
2021-04-16 03:36:07 +0000

    [Add]ajax-like

--- app/controllers/users/want_likes_controller.rb
+++ app/controllers/users/want_likes_controller.rb
@@ -2,17 +2,15 @@ class Users::WantLikesController < ApplicationController
   before_action :authenticate_user!
   
   def create
-    want_post = WantPost.find(params[:want_post_id])
-    want_like = current_user.want_likes.new(want_post_id: want_post.id)
+    @want_post = WantPost.find(params[:want_post_id])
+    want_like = current_user.want_likes.new(want_post_id: @want_post.id)
     want_like.save
-    redirect_to want_post_path(want_post)
   end
 
   def destroy
-    want_post = WantPost.find(params[:want_post_id])
-    want_like = current_user.want_likes.find_by(want_post_id: want_post.id)
+    @want_post = WantPost.find(params[:want_post_id])
+    want_like = current_user.want_likes.find_by(want_post_id: @want_post.id)
     want_like.destroy
-    redirect_to want_post_path(want_post)
   end
   
 end
